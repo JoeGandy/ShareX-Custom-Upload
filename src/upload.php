dea93f73a432d29b740a32d9966eb896ffaf0072
Benjamin Landry <31049059+benjamin238@users.noreply.github.com>
2019-03-16 14:36:22 -0400

    Prevent files from being overwritten.

--- src/upload.php
+++ src/upload.php
@@ -7,22 +7,26 @@ $uploadhost = $config['output_url'];
 $redirect = $config['redirect_url'];
 
 if ($_SERVER["REQUEST_URI"] == "/robot.txt") { die("User-agent: *\nDisallow: /"); }
- 
+
 if (isset($_POST['key'])) {
     if ($_POST['key'] == $key) {
         $parts = explode(".", $_FILES["d"]["name"]);
         if($config['enable_random_name']){
         $target = getcwd() . "/u/" .generateRandomName(end($parts),$config['random_name_length']);
         }else{
-          $target = getcwd() . "/u/" . $_POST['name'] . "." . end($parts);  
+          $target = getcwd() . "/u/" . $_POST['name'] . "." . end($parts);
         }
+        if (file_exists($target)) {
+            echo "The file already exists on the server.";
+        } else{
         if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
             $target_parts = explode("/u/", $target);
             echo $uploadhost . end($target_parts);
         } else {
             echo "Sorry, there was a problem uploading your file. (Ensure your directory has 777 permissions)";
         }
-    } else {
+    }}
+    else {
         header('Location: '.$redirect);
     }
 } else {
