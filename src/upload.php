cbdb3d609fada88c401d4844c5a35c426d43b3f9
Joe Gandy <joe@gandy.ws>
2019-11-16 11:55:02 +0000

    HOTFIX - Missing param

--- src/upload.php
+++ src/upload.php
@@ -13,7 +13,7 @@ if ('/robot.txt' === $_SERVER['REQUEST_URI']) {
 
 if (isset($_POST['key'])) {
     if ($_POST['key'] === $key) {
-        $target = get_file_target($_FILES['d']['name'], $_POST['name']);
+        $target = get_file_target($config, $_FILES['d']['name'], $_POST['name']);
 
         if (move_uploaded_file($_FILES['d']['tmp_name'], $target)) {
             $target_parts = explode('/u/', $target);
