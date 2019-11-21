1ca93926948b96eaabe9a8d878be5371cb689967
Jesper Tastesen <jesper@tastesen.dk>
2019-11-21 11:46:24 +0100

    makeid custom length

--- src/u/js/main.js
+++ src/u/js/main.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
 $('[data-toggle="tooltip"]').tooltip();
 
 
-function makeid() {
+function makeid(length = 33) {
     var result = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
-    for (var i = 0; i < 33; i++) {
+    for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
