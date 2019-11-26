416cb0d164aa6afebba26f5e848d1a9a231bdd93
Jesper Tastesen <jesper@tastesen.dk>
2019-11-26 21:46:35 +0100

    Update _FileNameTest.php

--- tests/_FileNameTest.php
+++ tests/_FileNameTest.php
@@ -21,9 +21,11 @@ final class FileNameTest extends TestCase
     {
         $orginal_file_name = "TestPngFile.png";
         $post_name = "07.53.17-08.11.19";
+        $enable_random_name = true;
+        $random_name_length = 8;
 
-        $correct = $orginal_file_name;
-        $test = get_file_target(['enable_random_name' => true], $orginal_file_name, $post_name);
+        $correct = $orginal_file_name;        
+        $test = get_file_target($random_name_length, $enable_random_name, $orginal_file_name, $post_name);
 
         $this->assertEquals(
             pathinfo($correct)['extension'],
@@ -36,9 +38,11 @@ final class FileNameTest extends TestCase
     {
         $orginal_file_name = "TestPngFile.png";
         $post_name = "07.53.17-08.11.19";
+        $enable_random_name = true;
+        $random_name_length = 8;
 
-        $correct = $orginal_file_name;
-        $test = get_file_target(['enable_random_name' => false], $orginal_file_name, $post_name);
+        $correct = $orginal_file_name;        
+        $test = get_file_target($random_name_length, $enable_random_name, $orginal_file_name, $post_name);
 
         $this->assertEquals(
             pathinfo($correct)['extension'],
